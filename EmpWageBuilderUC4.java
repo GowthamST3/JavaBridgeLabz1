15fdba87d282aac6cc15078298d01406350eb40c
GowthamST3 <gowthamgiri3@gmail.com>
2022-04-15 07:38:56 +0530

    Welcome to UC4

---
+++ EmpWageBuilderUC4.java
@@ -0,0 +1,23 @@
+package com.BridgeLabz;
+
+public class EmpWageBuilderUC4 {
+
+	public static void main(String[] args)
+	  {
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HR = 20;
+
+		int empHrs = 0;
+		int empWage = 0;
+
+		double empCheck = Math.floor(Math.random() *10) %2;
+		if(empCheck == IS_FULL_TIME)
+			empHrs=8;		
+		else
+			empHrs = 0;
+		
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("Employee Wage = "+empWage);
+	  }
+
+}
