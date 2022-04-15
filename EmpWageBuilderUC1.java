aa7f2f6808be8cce0ff0e21f191ba2a93365bf7a
GowthamST3 <gowthamgiri3@gmail.com>
2022-04-15 07:24:56 +0530

    Welcome to EmployeeWage

---
+++ EmpWageBuilderUC1.java
@@ -0,0 +1,11 @@
+package com.BridgeLabz;
+
+public class EmpWageBuilderUC1 
+{
+	public static void main(String[] args)
+	  {
+		  System.out.println("Welcome to Employee Wage Computation Program on Master "
+		  		+ "Branch  ");
+		
+	  }
+}
